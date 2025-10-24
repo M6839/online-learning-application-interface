fb0192c67e246e42a27117d2fa17b5d69998c7a8
Kiran Kumar <152247979+M6839@users.noreply.github.com>
2025-10-24 21:35:44 +0530

    Update contact information in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -114,9 +114,9 @@ const Footer = () => {
           <Col lg="3" md="6">
             <h6 className="fw-bold">Get in Touch</h6>
 
-            <p>Address: Sylhet, Bangladesh</p>
-            <p> Phone: +88 0123456789 </p>
-            <p>Email: example@gmail.com</p>
+            <p>Address: Nandyal,Andhra Pradesh</p>
+            <p> Phone:7997589153</p>
+            <p>Email: mkirankumar6839@gmail.com</p>
           </Col>
         </Row>
       </Container>
